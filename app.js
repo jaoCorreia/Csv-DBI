f800b2636ec902a878e837c559c62624df66821d
João Carlos <101333652+jaoCorreia@users.noreply.github.com>
2024-10-15 16:49:27 -0300

    csv menu

--- app.js
+++ app.js
@@ -1,50 +1,134 @@
+const readline = require('readline');
 const connectionController = require('./connectionController');
 const parser = require('csv-parser');
 const fs = require('fs');
-connectionController.getConnection();
+const logo = `+--------------------------------------------------------+
+|..%%%%....%%%%...%%..%%..........%%%%%...%%%%%...%%%%%%.|
+|.%%..%%..%%......%%..%%..........%%..%%..%%..%%....%%...|
+|.%%.......%%%%...%%..%%..%%%%%%..%%..%%..%%%%%.....%%...|
+|.%%..%%......%%...%%%%...........%%..%%..%%..%%....%%...|
+|..%%%%....%%%%.....%%............%%%%%...%%%%%...%%%%%%.|
+|........................................................|
++--------------------------------------------------------+`;
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-const query = {
-    atualizar: "UPDATE tb_uc SET uc_dataInstalacao= ?, uc_nio= ?, uc_status = 1 WHERE uc_numero = ?;",
-    buscar: "SELECT * FROM tb_uc WHERE uc_numero = ?",
-    atualizarComOldNio: "UPDATE tb_uc SET uc_dataInstalacao= ?, old_nio=?, uc_nio= ?, uc_status = 1 WHERE uc_numero = ?;"
+const querys = {
+    //Rota_leitura
+    inserirRota: "INSERT INTO `eletroresolve`.`tb_rota_leitura` (`data_inicio`, `data_final`,`etapa`) VALUES (?,?,?);", 
+    //UC
+    atualizarComOldNio: "UPDATE tb_uc SET uc_dataInstalacao= ?, old_nio=?, uc_nio= ?, uc_status = 1 WHERE uc_numero = ?;", 
+    buscarUcPorNumero: "SELECT * FROM tb_uc where uc_numero = ?", 
+    atualizarEtapa:"UPDATE tb_uc SET etapa = ? where uc_numero = ?"
 }
 
-const migracaoUc = {
-
-    inserirLatLong(data){
+const csvDbiDAO = {
+    atualizarUc(data){
         return new Promise((resolve, reject)=>{
-            try{
-                let date = new Date(data.DATA).toISOString().slice(0,10);
-
-                connectionController.conn.query(query.atualizarComOldNio,[date,data.OLD_NIO,data.NEW_NIO,data.UC],(err,res)=>{
-                    if(err){
-                        console.log(err);
-                    }   
-                    console.log(data);
-                    resolve(true);
-                });
-            }catch(err){
-                reject(err);
-            }
-        })
+            let date = new Date(data.DATA).toISOString().slice(0,10);
+            connectionController.conn.query(querys.atualizarComOldNio,[date,data.OLD_NIO,data.NEW_NIO,data.UC],(err,res)=>{
+                if(err) throw reject(new Error(err));
+                resolve(res);
+            });
+        });
     }, 
+
+    inserirRota(data){
+        return new Promise((resolve,reject)=> {
+                let dateInicio = new Date(data.INICIO).toISOString().slice(0,10);
+                let dateFinal = new Date(data.FINAL).toISOString().slice(0,10);
+                connectionController.conn.query(querys.inserirRota,[dateInicio,dateFinal,data.ETAPA],(err,res)=>{
+                    if(err) reject(new Error(err));
+                    resolve(res);
+                });
+        });
+    }, 
+
+    atualizarEtapa(data){
+        return new Promise((resolve,reject)=> {
+            connectionController.conn.query(querys.atualizarEtapa,[data.ETAPA,data.UC],(err,res)=>{
+                if(err) reject(new Error(err)); 
+                resolve(res);
+            });
+        });
+    },
+
+    buscarUcPorNumero(uc){
+        return new Promise((resolve,reject)=> {
+            connectionController.conn.query(querys.buscarUcPorNumero,[uc],(err,res)=>{
+                if(err) reject(new Error(err)); 
+                resolve(res);
+            });
+        });
+    },
 }
 
-async function dadosLista(){
-    let i = 1
-    fs.createReadStream('data.csv').pipe(parser({separator: ';',skipLines: 1}))
+function mostrarLoader() {
+    const frames = ['𓃉𓃉𓃉', '𓃉𓃉∘', '𓃉∘°', '∘°∘', '°∘𓃉', '∘𓃉𓃉'];
+    let i = 0;
+    return setInterval(() => {
+      readline.cursorTo(process.stdout, 0);
+      process.stdout.write(`Aguarde ${frames[i++]}`);
+      i %= frames.length;
+    }, 200); 
+}
+  
+ function csvDbiController(arquivo, op){
+    let i = 1;
+    let data = [];
+    const opcao= op;
+    fs.createReadStream(`${arquivo}.csv`).pipe(parser({separator: ';',skipLines: 1}))
     .on('data', async (dadoLinha) => {    
-
         console.log(dadoLinha);
-        let inserir = await migracaoUc.inserirLatLong(dadoLinha);    
-
-        if(inserir){
-            console.log("inserido "+i);
-            i++; 
-        }
-
+        data.push(dadoLinha)
     }).on('error',error=>{throw new Error (error)})
-    .on('end',()=>console.log("Arquivo Lido"));
+    .on('end',  ()=>{
+
+        if(opcao == 1){
+            let loader = mostrarLoader();
+            let dataPromisse = data.map(d => csvDbiDAO.atualizarUc(d));
+            Promise.all(dataPromisse).then((res) => {
+                clearInterval(loader);
+                readline.clearLine(process.stdout,0);
+                const audic = new Audic('Untitled-12_2.mp3');
+                audic.play();
+                console.log(`\n${res.length} UCs atualizadas`);
+            });
+
+        }else if(opcao == 2){
+            let loader = mostrarLoader();
+            let dataPromisse = data.map(d => csvDbiDAO.inserirRota(d));
+            Promise.all(dataPromisse).then((res)=>{
+                clearInterval(loader);
+                readline.clearLine(process.stdout,0);
+                console.log(`\n${res.length} Rotas inseridas`)
+            });
+
+        }else if(opcao == 3){
+            let loader = mostrarLoader();
+            let dataPromisse = data.map(d => csvDbiDAO.atualizarEtapa(d));
+            Promise.all(dataPromisse).then((res)=>{
+                clearInterval(loader);
+                readline.clearLine(process.stdout,0);
+                console.log(`\n${res.length} UCs atualizadas`)
+            });
+        }
+    });
+
 }
 
-dadosLista();
+
+rl.question(logo+'\nMenu de opção Csv-DBI\n1.Atualizar status uc (com old_nio)\n2.Inserir rota de leitura\n3.Inserir Etapa em Uc\n', (op) => {
+    const opcao = op; 
+    if(parseInt(opcao)){
+        rl.question('Insira o nome do arquivo:\n',async (arquivo)=>{
+            rl.close();   
+            await connectionController.getConnection();
+            csvDbiController(arquivo,opcao);
+        });
+    }else{
+        throw new Error("Opção Invalida!");
+    }
+});

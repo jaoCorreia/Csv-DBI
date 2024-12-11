3e055fd87832c1835621a5dcaa6b289284287fd6
João Carlos <101333652+jaoCorreia@users.noreply.github.com>
2024-12-11 13:47:20 -0300

    atualizar-status 6

--- connectionController.js
+++ connectionController.js
@@ -9,8 +9,11 @@ const connectionController = {
     }),
     
     getConnection(){ 
-        this.conn.connect((err, res)=>{
-            if(err) throw new Error (err);
+        this.conn.connect((err, res) => {
+            if (err) {
+                console.error('Error connecting to database:', err);
+                throw err; // Or handle the error differently
+            }
             console.log("Connected");
         });
     }, 
